268c6e137af6ea605a1b5cca3974c73afb159654
Sirisha Panuganti <123522834+sirishapanu03@users.noreply.github.com>
2024-02-18 22:22:09 -0600

    events code

---
+++ GDP_Project/app/src/main/java/com/example/studenthub/Events/EventsAdapter.java
@@ -0,0 +1,81 @@
+package com.example.studenthub.events;
+
+
+import static com.example.studenthub.utils.Utils.dateFormat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.studenthub.R;
+
+import java.util.List;
+
+
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsViewHolder> {
+
+    private OnItemClickListener onItemClickListener;
+
+    private final List<Event> dataList;
+    private final Context context;
+
+    public EventsAdapter(Context context, List<Event> list) {
+        this.context = context;
+        this.dataList = list;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public EventsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_event, parent, false);
+        return new EventsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EventsViewHolder holder, int position) {
+        Event currentItem = dataList.get(position);
+        holder.tvDescription.setText(currentItem.getDescription());
+        holder.tvName.setText(currentItem.getTitle());
+        String date = dateFormat(currentItem.getDate());
+        holder.tvDate.setText("" + date);
+        holder.tvLocation.setText(currentItem.getLocation());
+
+        Glide.with(context).load(currentItem.getImageUrl()).into(holder.iv);
+
+        holder.btRSVP.setOnClickListener(view -> onItemClickListener.onItemClick(currentItem));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+
+    static class EventsViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvName, tvDescription, tvDate, tvLocation;
+        public ImageView iv;
+        public Button btRSVP;
+
+        public EventsViewHolder(View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvDate = itemView.findViewById(R.id.tvDate);
+            iv = itemView.findViewById(R.id.iv);
+            btRSVP = itemView.findViewById(R.id.btRSVP);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+        }
+    }
+}
